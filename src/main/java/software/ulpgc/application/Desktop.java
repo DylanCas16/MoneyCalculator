05e0d299c6103db03659d80dab5aa05a96de4d0e
DylanCas16 <dylancastrillon@outlook.com>
2025-12-29 17:01:03 +0000

    Created first view for GUI money calculator

--- src/main/java/software/ulpgc/application/Desktop.java
+++ src/main/java/software/ulpgc/application/Desktop.java
@@ -1,7 +1,41 @@
 package software.ulpgc.application;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Desktop extends JPanel {
+public class Desktop extends JFrame {
+    public Desktop() {
+        setTitle("Money Calculator");
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
+        JPanel screen = new JPanel();
+        screen.setLayout(new GridLayout(1, 2));
+
+        createInputArea(screen);
+        createOutputArea(screen);
+
+        this.add(screen);
+    }
+
+    private void createOutputArea(JPanel screen) {
+        JPanel panel = new JPanel();
+        createTextField(panel, false);
+        panel.add(new JButton("Clear"));
+        screen.add(panel);
+    }
+
+    private void createInputArea(JPanel screen) {
+        JPanel panel = new JPanel();
+        createTextField(panel, true);
+        panel.add(new JButton("Calculate"));
+        screen.add(panel);
+    }
+
+    private void createTextField(JPanel panel, boolean editable) {
+        JTextField textField = new JTextField(15);
+        textField.setEditable(editable);
+        panel.add(textField);
+    }
 }
